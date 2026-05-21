bb631259ac9d237f699282a3f76d86b69329ec7e
92Jhonna92 <92Jhonna92@gmail.com>
2026-05-20 22:50:20 -0400

    third

--- Inicio.py
+++ Inicio.py
@@ -28,27 +28,56 @@ def login():
 ##Funcion Agregar producots
 def agregar_producto():
     print("Agregar Producto")
-    nombreP=input("Ingrese nombre del producto: ")
-    precioP=float(input("Ingrese precio del producto: "))
-    stockP=int(input("Ingrese stock del producto: "))
+    nombre=input("Ingrese nombre del producto: ")
+    precio=float(input("Ingrese precio del producto: "))
+    stock=int(input("Ingrese stock del producto: "))
     producto={
-        "nombre":nombreP,
-        "precio":precioP,
-        "stock":stockP
+        "nombre":nombre,
+        "precio":precio,
+        "stock":stock
     }
-    inventario.append(producto)
-print("Producto guardado exitosamente!")
+    inventario.append(producto) ## .append es para agregar un elemento al final de la lista
+    print("Producto guardado exitosamente!")
 
 ##Funcion Ver Inventario de productos
 def ver_inventario():
-    print("Inventario de Productos")
+    print("===Inventario de Productos===")
     if len(inventario)==0:
         print("No hay productos registrados")
         return
     for i, producto in enumerate(inventario):
-        print(f"Id: {i+1} - Nombre: {producto['nombreP']} - Precio Bs: {producto['precioP']} - Stock: {producto['stockP']}") ##Variables de prdocuto
+         print(f"Id: {i+1} - Nombre: {producto['nombre']} - Precio Bs: {producto['precio']} - Stock: {producto['stock']}") ##Variables de prdocuto
+         print("==============================================")
 
+##Funcion Modificar productos
+def modificar_producto():
+    ver_inventario()
+    if len(inventario)==0:
+        return
+    indice=int(input("Ingrese ID del producto a modificar: "))
+    if indice>=0 and indice<len(inventario):
+        print("Ingrese los nuevos datos: ")
+        new_nombre=input("Nuevo nombre del producto: ")
+        new_precio=float(input("Nuevo precio del producto: "))
+        new_stock=int(input("Nuevo stock del producto: "))
+        inventario[indice]["nombre"]= new_nombre
+        inventario[indice]["precio"]=new_precio
+        inventario[indice]["stock"]=new_stock
+        print("Producto modificado correctamente!")
+    else:
+        print("Producto no valido")
 
+##Funcion Eliminar Producto
+def eliminar_producto():
+    ver_inventario()
+    if len(inventario)==0:
+        return
+    indice=int(input("Ingrese ID del producto a Eliminar"))
+    if indice>=0 and indice<len(inventario):
+        producto_eliminado=inventario.pop(indice) ## .pop para eliminar de la lista(aca se pone la posicion)
+        print(f"Producto Eliminado: {producto_eliminado}")
+    else:
+        print("Producto no Valido")
 
 
 ##Menus---------------------------------
@@ -62,7 +91,7 @@ def menu_inventario():
         print("2.Modificar Producto")
         print("3.Eliminar Producto")
         print("4.Ver Inventario")
-        print("5.Salir")
+        print("5.Atras")
         opI=input("Seleccione una opcion: ")
         if opI=="1":
             agregar_producto()
@@ -115,7 +144,7 @@ while True:
     elif op=="1":
         login()
     elif op=="3":
-        print("Adios")
+        print("Cesion cerrada.!")
         break
     else:
         print("Opcion invalida, intente nuevamente")
